4272a76a4b16fac69d937c08706f195623b89d80
bekir <bekirtokac@gmail.com>
2025-01-05 02:14:44 +0300

    Changes Has Made

---
+++ layouts/MainLayout.vue
@@ -0,0 +1,26 @@
+<template>
+    <MainLayout>
+      <div>
+        <Navbar />
+        <Component1 />
+        <Component2 />
+      </div>
+    </MainLayout>
+  </template>
+  
+  <script>
+  import MainLayout from '~/layouts/MainLayout.vue';
+  import Navbar from '~/components/Navbar.vue';
+  import Component1 from '~/components/Component1.vue';
+  import Component2 from '~/components/Component2.vue';
+  
+  export default {
+    components: {
+      MainLayout,
+      Navbar,
+      Component1,
+      Component2,
+    },
+  };
+  </script>
+  
